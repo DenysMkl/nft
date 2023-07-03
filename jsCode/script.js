c6692f6b631a190f80964104f58eb89dc9410205
DenysMkl <den.mikliaiev@gmail.com>
2023-07-03 16:35:20 +0300

    add new block

---
+++ jsCode/script.js
@@ -0,0 +1,12 @@
+parent = document.querySelector('.sort-by-date')
+
+parent.addEventListener('click', (event)=>{
+    
+    elements = Array.from(parent.children)
+    targetElem = event.target.closest('.sort-by-date .but')
+    if (targetElem != null){
+        elements.forEach(elem => {
+            elem.classList.toggle('active', targetElem == elem)
+        });
+    }
+})
