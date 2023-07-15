762035d45dcf79eed1796ae82f68bce3e8e4eb9d
DenysMkl <den.mikliaiev@gmail.com>
2023-07-15 23:25:33 +0300

    create attach-photo script

--- jsCode/attach-photo.js
+++ jsCode/attach-photo.js
@@ -1,2 +1,21 @@
-inp = document.querySelector('#user-bg')
+const inp = document.querySelector('#user-bg');
+const avatar = document.querySelector('#user-avatar');
+const cur_bg_image = document.querySelector('.curr-bg-img');
+const cur_avatar_image = document.querySelector('.avatar-img');
 
+inp.addEventListener('change', (event) => {
+    replace_image(cur_bg_image, inp)
+});
+
+avatar.addEventListener('change', (event) => {
+    replace_image(cur_avatar_image, avatar)
+});
+
+function replace_image(image_block, elem) {
+    const file_of_img = new FileReader();
+    file_of_img.readAsDataURL(elem.files[0]);
+    file_of_img.addEventListener('load', () => {
+        const url = file_of_img.result;
+        image_block.src = url;
+    });
+}
