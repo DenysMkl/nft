40665554fae7ee7edadf390f8985b26026fd8c6b
DenysMkl <den.mikliaiev@gmail.com>
2023-07-18 15:00:41 +0300

    add user-prof page

--- jsCode/line-check.js
+++ jsCode/line-check.js
@@ -2,19 +2,22 @@
 const bounce = document.querySelector('.add-info-block')
 const line = document.querySelector('.active-line')
 const list_block = document.querySelector('.add-info-nav')
+const list_items = Array.from(list_block.children)
+
+line.style.left = `${list_items[0].getBoundingClientRect().x - bounce.getBoundingClientRect().x}px`
+line.style.width = `${list_items[0].offsetWidth}px`
 
 list_block.addEventListener('click', event=>{
-    list_items = Array.from(list_block.children)
-
     check = event.target.closest('.add-info-item')
-
+    console.log(check)
     if (check){
+        line.style.left = `0px`
         list_items.forEach(elem => {
             elem.classList.toggle('active', check == elem)
         });
+        var rect = check.getBoundingClientRect()
+        var rect_bounce = bounce.getBoundingClientRect()
+        line.style.transform = `translateX(${rect.x - rect_bounce.x}px)`
+        line.style.width = `${check.offsetWidth}px`
     }
-    var rect = check.getBoundingClientRect()
-    var rect_bounce = bounce.getBoundingClientRect()
-    line.style.transform = `translateX(${rect.x - rect_bounce.x}px)`
-    line.style.width = `${check.offsetWidth}px`
 })
