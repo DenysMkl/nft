50b2b49dac897efbd7ca452e8e540411e41a830e
DenysMkl <den.mikliaiev@gmail.com>
2023-07-25 20:10:31 +0300

    create main page by using django

--- nft/main/views.py
+++ nft/main/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(req):
-    return HttpResponse('<h1>Hello world!</h1>')
+    return render(req, 'main/index.html', context={'isMainPage': True})
